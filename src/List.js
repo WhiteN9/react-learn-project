a40a8e3d0cec62bb6ff8d513a24ffb1d295974f4
White Nguyen <whitenguyen0909@gmail.com>
2022-05-03 21:18:56 -0600

    list and format

--- src/List.js
+++ src/List.js
@@ -22,6 +22,49 @@ function MakeList() {
   return <ul>{listItems}</ul>;
 }
 
+//Basic List Component
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) =>
+//     <li>{number}</li>
+//   );
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
+// const numbers = [1, 2, 3, 4, 5];
+
+//Embedding map() in JSX
+//INLINE 2 FUNCTIONS RETURN, BETTER BECAUSE INDEX KEYS ARE IN LISTITEM ELEMENTS INSTEAD OF LI ELEMENTS
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   return (
+//     <ul>
+//       {numbers.map((number) =>
+//         <ListItem key={number.toString()}
+//                   value={number} />
+//       )}
+//     </ul>
+//   );
+// }
+
+//Embedding map() in JSX
+//IN-LINE 1 FUNCTION RETURN, NOT GOOD BECAUSE INDEX KEYS IS INSIDE LI ELEMENTS
+// function HobbyList({hobbies}) {
+//   if (hobbies.length ===0) return null;
+//   return (
+//     <div className="hobbies">
+//       <h4>Hobbies</h4>
+//       <ul>
+//         {hobbies.map((hobby,index) => <li key={index}>{hobby}</li>
+//         )}
+//       </ul>
+//     </div>
+
+//   )
+//   // You must use this heading for this component
+// }
+
 // https://reactjs.org/docs/lists-and-keys.html
 // It's recommended to use stable IDs for rendered items instead of item index.
 // Keys only make sense in the context of the surrounding array?
---
+++ src/setupTests.js
@@ -0,0 +1,4 @@
+/* The jest-dom library adds custom matchers for performing assertions 
+on DOM nodes (such as toHaveTextContent() or toBeInTheDocument()), 
+which you will learn about later on in this lesson.*/
+import "@testing-library/jest-dom"
